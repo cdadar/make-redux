64072373f8c7fa2433383a66daeec24d96e139b8
jiarenlu <gunbanmi@gmail.com>
2017-05-22 15:00:33 +0800

    Share the structure of the object to improve performance

--- src/index.js
+++ src/index.js
@@ -9,21 +9,27 @@ const appState = {
     }
 }
 
-function renderApp(appState) {
-    renderTitle(appState.title)
-    renderContent(appState.content)
+function renderApp(newAppState, oldAppState = {}) {
+    if (newAppState === oldAppState) return
+    console.log('render app...')
+    renderTitle(newAppState.title)
+    renderContent(newAppState.content)
 }
 
-function renderTitle(title) {
+function renderTitle(newTitle, oldTitle = {}) {
+    if (newTitle === oldTitle) return
+    console.log('render title...')
     const titleDOM = document.getElementById('title')
-    titleDOM.innerHTML = title.text
-    titleDOM.style.color = title.color
+    titleDOM.innerHTML = newTitle.text
+    titleDOM.style.color = newTitle.color
 }
 
-function renderContent(content) {
+function renderContent(newContent, oldContent) {
+    if (newContent === oldContent) return
+    console.log('render content...')
     const contentDOM = document.getElementById('content')
-    contentDOM.innerHTML = content.text
-    contentDOM.style.color = content.color
+    contentDOM.innerHTML = newContent.text
+    contentDOM.style.color = newContent.color
 }
 
 function dispath(action) {
@@ -42,23 +48,44 @@ function dispath(action) {
 function stateChanger(state, action) {
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            state.title.text = action.text
-            break
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    text: action.text
+                }
+            }
         case 'UPDATE_TITLE_COLOR':
-            state.title.color = action.color
-            break
+            return {
+                ...state,
+                title: {
+                    ...state.title,
+                    color: action.color
+                }
+            }
         default:
-            break
+            return state
     }
 
 }
+
+// function createStore(state, stateChanger) {
+//     const listeners = []
+//     const subscribe = (listener) => listeners.push(listener)
+//     const getState = () => state
+//     const dispatch = (action) => {
+//         stateChanger(state, action)
+//         listeners.forEach((listener) => listener())
+//     }
+//     return { getState, dispatch, subscribe }
+// }
 
 function createStore(state, stateChanger) {
     const listeners = []
     const subscribe = (listener) => listeners.push(listener)
     const getState = () => state
     const dispatch = (action) => {
-        stateChanger(state, action)
+        state = stateChanger(state, action) // 覆盖原对象
         listeners.forEach((listener) => listener())
     }
     return { getState, dispatch, subscribe }
@@ -70,7 +97,12 @@ function createStore(state, stateChanger) {
 // renderApp(appState)
 
 const store = createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState()))
+let oldState = store.getState()
+store.subscribe(() => {
+    const newState = store.getState()
+    renderApp(newState, oldState)
+    oldState = newState
+})
 
 renderApp(store.getState())
 store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React js 小书》' })
